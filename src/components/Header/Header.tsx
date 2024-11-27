7e91e9bfbd953f25cdd848e51622cb9f9389e6fd
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2024-11-27 09:52:17 +0200

    Initial commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="flex flex-col items-center gap-4">
+      <h1 className="text-4xl font-bold">logo</h1>
+      <ul className="flex gap-4">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
